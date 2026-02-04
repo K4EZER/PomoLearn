5d82f19aee35cd0fcb862adaef6205284cf91242
K4EZER <125785378+K4EZER@users.noreply.github.com>
2026-02-04 19:15:47 +0800

    changed import * to necessary imports

--- PomoLearn.py
+++ PomoLearn.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import *
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
 from PyQt5.QtGui import QIcon, QFont, QPixmap
 from PyQt5.QtCore import Qt
 class MainWindow(QMainWindow):
